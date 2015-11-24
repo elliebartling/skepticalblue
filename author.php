72020e704ce610d947b1f254dbaeb19de03b08ad
Ellie Bartling <ellenbartling@gmail.com>
2015-11-23 19:12:31 -0600

    Responsive styling fixes

--- author.php
+++ author.php
@@ -53,30 +53,9 @@ add_action( 'pre_get_posts', 'be_change_event_posts_per_page' );
  *
  */
 function be_change_event_posts_per_page( $query ) {
-    $query->set( 'posts_per_page', '18' );
+    $query->set( 'posts_per_page', '6' );
 }
 
-
-
-function load_more() {
-  if(is_author()){
-      $author = get_the_author_meta('ID');
-      echo do_shortcode('[ajax_load_more author="'.$author.'" post_type="post" scroll="false" images_loaded="true" button_label="More" css_classes="post content brick wp_rss_retriever_item"]');
-  }
-  if(is_category()){
-      $cur_cat_id = get_cat_id( single_cat_title("",false) ); 
-      $category = get_cat_slug($cur_cat_id);  ?>
-      <h1><span>Category:</span> <?php echo single_cat_title('', false );?> </h1>
-      <?php
-      echo do_shortcode('[ajax_load_more category="'.$category.'"post_type="post" scroll="false" images_loaded="true" button_label="More" css_classes="post one-half teaser"]');
-  }
-  if(is_tag()){ ?>
-      <h1><span>Tag:</span> <?php echo single_cat_title('', false );?></h1>
-      <?php
-      $tag = get_query_var('tag'); 
-      echo do_shortcode('[ajax_load_more tag="'.$tag.'"post_type="post" scroll="false" images_loaded="true" button_label="More" css_classes="post one-half teaser"]');
-  }
-}
 
 function filter_buttons() {
   ?>
