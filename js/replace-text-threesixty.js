b2e37fe41856da79f7b1114d09ae0921b81ed601
Ellen Bartling <ellenbartling@gmail.com>
2016-04-03 12:08:11 -0500

    Removed quotes from 360 titles, fixed first-post styling, trying a patch for duplicated magnifying glass

--- js/replace-text-threesixty.js
+++ js/replace-text-threesixty.js
@@ -11,4 +11,18 @@ jQuery(document).ready(function($) {
 	        $(this).remove (); //-- Kill the <br>
 	    }
 	} );
+
+	// Remove IFTTT's quotation marks around
+	// the title of ThreeSixty posts
+	$('#sidebar-ThreeSixty-blog a.wp_rss_retriever_title').each( function() {
+		var rssTitle = $(this).text();
+		rssTitle = rssTitle.slice(1,-1);
+
+		$(this).text(function() {
+			return rssTitle;
+		});
+		
+		console.log(rssTitle);
+
+	});
 });
